c459d172e117c5d2be711e7538fb69e6c5c4549e
agusgonz <agustingendeiza@gmail.com>
2023-04-24 11:59:27 -0300

    inicial Commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,11 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    colors: {
+      primaryCol: '#1B262C',
+      darkBlueCol: '#0F4C75',
+      blueCol: '#3282B8',
+      lightBlueCol: '#BBE1FA',
     },
   },
   plugins: [],
